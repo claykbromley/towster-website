fe23fe54b682ab9b940f3471268cb9433e7d6d7b
claykbromley <clay.k.bromley@gmail.com>
2023-10-13 21:42:03 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -185,7 +185,7 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Award-winning development studio based in Denmark.
+            bio-winning development studio based in Denmark.
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
             We are a development studio working at the intersection of design
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -125,7 +125,7 @@ export function Footer() {
             <Logo className="h-8" fillOnHover />
           </Link>
           <p className="text-sm text-neutral-700">
-            © Studio Agency Inc. {new Date().getFullYear()}
+             @ Towster Corporation. {new Date().getFullYear()}
           </p>
         </div>
       </FadeIn>
