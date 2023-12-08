52c599b15aafdc34df9441dcc45cc401080281ef
One <50631904+coffeewithone@users.noreply.github.com>
2023-12-07 22:42:34 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -185,7 +185,7 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            bio-winning development studio based in Denmark.
+            Towster
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
             We are a development studio working at the intersection of design
