7c9b94ad34f4b0a8c19131e61940745fe22b9b7a
One <50631904+coffeewithone@users.noreply.github.com>
2023-12-18 00:19:13 -0500

    contavt updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -96,45 +96,34 @@ function ContactForm() {
 function ContactDetails() {
   return (
     <FadeIn>
-      <h2 className="font-display text-base font-semibold text-neutral-950">
-        Our offices
-      </h2>
-      <p className="mt-6 text-base text-neutral-600">
-        Prefer doing things in person? We don’t but we have to list our
-        addresses here for legal reasons.
-      </p>
-
-      <Offices className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2" />
-
       <Border className="mt-16 pt-16">
         <h2 className="font-display text-base font-semibold text-neutral-950">
           Email us
         </h2>
         <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
-          {[
-            ['Careers', 'careers@studioagency.com'],
-            ['Press', 'press@studioagency.com'],
-          ].map(([label, email]) => (
-            <div key={email}>
-              <dt className="font-semibold text-neutral-950">{label}</dt>
-              <dd>
-                <Link
-                  href={`mailto:${email}`}
-                  className="text-neutral-600 hover:text-neutral-950"
-                >
-                  {email}
-                </Link>
-              </dd>
-            </div>
-          ))}
+          {[['Contact', 'yousef.ahmed@towstercorp.com']].map(
+            ([label, email]) => (
+              <div key={email}>
+                <dt className="font-semibold text-neutral-950">{label}</dt>
+                <dd>
+                  <Link
+                    href={`mailto:${email}`}
+                    className="text-neutral-600 hover:text-neutral-950"
+                  >
+                    {email}
+                  </Link>
+                </dd>
+              </div>
+            ),
+          )}
         </dl>
       </Border>
 
       <Border className="mt-16 pt-16">
-        <h2 className="font-display text-base font-semibold text-neutral-950">
+        {/* <h2 className="font-display text-base font-semibold text-neutral-950">
           Follow us
         </h2>
-        <SocialMedia className="mt-6" />
+        <SocialMedia className="mt-6" /> */}
       </Border>
     </FadeIn>
   )
@@ -154,7 +143,7 @@ export default function Contact() {
 
       <Container className="mt-24 sm:mt-32 lg:mt-40">
         <div className="grid grid-cols-1 gap-x-8 gap-y-24 lg:grid-cols-2">
-          <ContactForm />
+          {/* <ContactForm /> */}
           <ContactDetails />
         </div>
       </Container>
