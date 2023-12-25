45d0e1057ddbda41482b8dd292277e2b6d699a59
claykbromley <clay.k.bromley@gmail.com>
2023-12-25 12:28:46 -0500

    Change icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -171,7 +171,7 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Mobile Hyperbaric Solution
+            Mobile Hyperbaric Solutions
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
             Towster Corporation is specializing in innovative medical
